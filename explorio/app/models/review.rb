f037adfb94ddbbe73efaed5401625547fb1e94de
highlydubious <alexdubelko@gmail.com>
2014-09-30 16:56:17 -0700

    Controller were just made

--- explorio/app/models/review.rb
+++ explorio/app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ActiveRecord::Base
+  belongs_to :curriculum
+  belongs_to :user
 end
--- explorio/config/routes.rb
+++ explorio/config/routes.rb
@@ -1,4 +1,48 @@
 Rails.application.routes.draw do
+  get 'curriculums/index'
+
+  get 'curriculums/show'
+
+  get 'curriculums/new'
+
+  get 'curriculums/create'
+
+  get 'curriculums/edit'
+
+  get 'curriculums/update'
+
+  get 'curriculums/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'groups/index'
+
+  get 'groups/show'
+
+  get 'groups/new'
+
+  get 'groups/create'
+
+  get 'groups/edit'
+
+  get 'groups/update'
+
+  get 'groups/destroy'
+
+  get 'pages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
