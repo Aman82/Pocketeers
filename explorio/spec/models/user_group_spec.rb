9e368d87cc5b643a4cf253445a5195e32493d54c
Aman82 <jotdhillon@gmail.com>
2014-10-10 16:58:33 -0700

    user profile and some tests

--- explorio/spec/models/user_group_spec.rb
+++ explorio/spec/models/user_group_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UserGroup, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should belongs to users" do
+    user = build(:user, first_name = nil)
+    expect(user).to be_invalid
+  end
+
+  it "should belongs to group" do
+    user = build(:user, first_name = nil)
+    expect(user).to be_invalid
+  end
+
+
 end
