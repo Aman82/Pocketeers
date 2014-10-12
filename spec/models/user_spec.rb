03bc57196881d600d133be5b12c0091525dd34fa
Aman82 <jotdhillon@gmail.com>
2014-10-12 09:52:15 -0700

    working on rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  it "should have username" do
-    user = build(:user, first_name = nil)
-    expect(user).to be_invalid
-  end  
 
-  it "should have first_name" do
-    user = build(:user, first_name = nil)
-    expect(user).to be_invalid
-  end  
-
-  it "should have last_name" do
-  	user = build(:user, first_name = nil)
-  	expect(user).to be_invalid
-  end
 
 
 end
