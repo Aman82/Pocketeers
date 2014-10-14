df4f436161ad3c07711392ddfc99c09dc81b5fb3
benan789 <benjamin_an@yahoo.com>
2014-10-13 19:55:51 -0700

    some more minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
   def create
   @user = User.new(params.require(:user).permit(:username, :first_name, :last_name, :email, :email_confirmation, :password, :password_confirmation, :avatar))
   if @user.save
-    flash[:notice] = "New User Created!"
+    flash[:notice] = "New User Created! Please Sign In."
     # log the user in
-    redirect_to sessions_path
+    redirect_to root_path
   else
     render 'new'
   end
